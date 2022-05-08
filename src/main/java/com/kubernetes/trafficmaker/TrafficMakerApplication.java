b9c584b2d0ba772bf4f18540582b06b30ef7617d
KimDoubleB <tree9295@gmail.com>
2022-05-08 21:10:38 +0900

    feat: First commit - init project

---
+++ src/main/java/com/kubernetes/trafficmaker/TrafficMakerApplication.java
@@ -0,0 +1,13 @@
+package com.kubernetes.trafficmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kubernetes/trafficmaker/TrafficMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubernetes.trafficmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
