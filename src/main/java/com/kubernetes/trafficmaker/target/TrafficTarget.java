fff32b17727dc435a3ab59ff3fa642411c69e3ac
KimDoubleB <tree9295@gmail.com>
2022-05-08 23:44:54 +0900

    feat: added requestToTargetTask method
    return request to targetUri Runnable instance

--- src/main/java/com/kubernetes/trafficmaker/target/TrafficTarget.java
+++ src/main/java/com/kubernetes/trafficmaker/target/TrafficTarget.java
@@ -4,10 +4,27 @@ import io.fabric8.kubernetes.api.model.Namespaced;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.model.annotation.Group;
 import io.fabric8.kubernetes.model.annotation.Version;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.net.URI;
 
 @Group("bb.traffic-maker.com")
 @Version("v1alpha1")
+@Slf4j
 public class TrafficTarget
         extends CustomResource<TrafficTargetSpec, TrafficTargetStatus>
         implements Namespaced {
+
+    public Runnable requestToTargetTask(WebClient webClient, URI targetUri) {
+        return () -> {
+            log.debug("Request to target {}", targetUri);
+            webClient.get()
+                    .uri(targetUri)
+                    .retrieve()
+                    .toBodilessEntity()
+                    .subscribe();
+        };
+    }
+
 }
