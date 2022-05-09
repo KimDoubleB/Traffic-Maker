9ebf958eee5405b5fc18a57eed06d4ac183ae29f
KimDoubleB <tree9295@gmail.com>
2022-05-10 00:58:17 +0900

    feat: update TrafficTarget CRD
    - define Status: tarffic target current status
    - support short name
    - support additionalPrinterColumns field

--- src/main/java/com/kubernetes/trafficmaker/target/TrafficTargetStatus.java
+++ src/main/java/com/kubernetes/trafficmaker/target/TrafficTargetStatus.java
@@ -1,4 +1,10 @@
 package com.kubernetes.trafficmaker.target;
 
-public record TrafficTargetStatus() { // 업데이트할 수 없음. request 시, sync 하는 것도 아니니깐...
+public record TrafficTargetStatus(Status status) {
+
+    public enum Status {
+        SCHEDULING,
+        FAILURE,
+    }
+
 }
