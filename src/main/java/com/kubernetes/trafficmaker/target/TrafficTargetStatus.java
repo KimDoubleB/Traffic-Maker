bf5ce9517485014bbbd91357581790c37e1e831a
KimDoubleB <tree9295@gmail.com>
2022-05-18 21:27:22 +0900

    feat(#2): added `isScheduledStatus` static method for checking TrafficTarget status is scheduled

--- src/main/java/com/kubernetes/trafficmaker/target/TrafficTargetStatus.java
+++ src/main/java/com/kubernetes/trafficmaker/target/TrafficTargetStatus.java
@@ -2,6 +2,11 @@ package com.kubernetes.trafficmaker.target;
 
 public record TrafficTargetStatus(Status status) {
 
+    public static boolean isScheduledStatus(TrafficTargetStatus trafficTargetStatus) {
+        return trafficTargetStatus != null
+               && trafficTargetStatus.status() == TrafficTargetStatus.Status.SCHEDULING;
+    }
+
     public enum Status {
         SCHEDULING,
         FAILURE,
