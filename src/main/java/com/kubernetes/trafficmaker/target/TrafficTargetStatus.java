7afcdda4e0fcc0b61b7cfad504c32c84f498184c
KimDoubleB <tree9295@gmail.com>
2022-05-18 22:28:23 +0900

    feat(#2): added UPDATING status

--- src/main/java/com/kubernetes/trafficmaker/target/TrafficTargetStatus.java
+++ src/main/java/com/kubernetes/trafficmaker/target/TrafficTargetStatus.java
@@ -4,12 +4,18 @@ public record TrafficTargetStatus(Status status) {
 
     public static boolean isScheduledStatus(TrafficTargetStatus trafficTargetStatus) {
         return trafficTargetStatus != null
-               && trafficTargetStatus.status() == TrafficTargetStatus.Status.SCHEDULING;
+               && trafficTargetStatus.status() == Status.SCHEDULING;
+    }
+
+    public static boolean isFailureStatus(TrafficTargetStatus trafficTargetStatus) {
+        return trafficTargetStatus != null
+               && trafficTargetStatus.status() == Status.FAILURE;
     }
 
     public enum Status {
         SCHEDULING,
         FAILURE,
+        UPDATING,
     }
 
 }
