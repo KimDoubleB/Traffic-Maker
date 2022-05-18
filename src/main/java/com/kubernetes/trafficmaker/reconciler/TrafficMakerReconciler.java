2a42e7ec2fda766ed78f45002d94b0944653454a
KimDoubleB <tree9295@gmail.com>
2022-05-18 21:32:40 +0900

    refactor(#2): update schedule task when already scheduled `TrafficTarget` manifest reconcile

--- src/main/java/com/kubernetes/trafficmaker/reconciler/TrafficMakerReconciler.java
+++ src/main/java/com/kubernetes/trafficmaker/reconciler/TrafficMakerReconciler.java
@@ -2,6 +2,8 @@ package com.kubernetes.trafficmaker.reconciler;
 
 import com.kubernetes.trafficmaker.schedule.TrafficScheduler;
 import com.kubernetes.trafficmaker.target.TrafficTarget;
+import com.kubernetes.trafficmaker.target.TrafficTargetStatus;
+import com.kubernetes.trafficmaker.target.TrafficTargetStatus.Status;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.DeleteControl;
@@ -22,10 +24,29 @@ public class TrafficMakerReconciler implements Reconciler<TrafficTarget> {
     private final WebClient webClient;
     private final TrafficScheduler trafficScheduler;
 
+    @SuppressWarnings("ReactiveStreamsUnusedPublisher")
     @Override
     public UpdateControl<TrafficTarget> reconcile(TrafficTarget trafficTarget, Context context) {
         log.debug("Reconcile by trafficTarget {}", trafficTarget);
-        scheduleTask(trafficTarget);
+
+        var taskName = trafficTarget.getMetadata().getName();
+        var httpTargetSpec = trafficTarget.getSpec().http();
+        var httpRequestMono = httpTargetSpec.toRequestMono(webClient);
+        var period = DurationStyle.detectAndParse(trafficTarget.getSpec().rate());
+
+        if (TrafficTargetStatus.isScheduledStatus(trafficTarget.getStatus())) {
+            trafficScheduler.updateFixedRateSchedule(taskName, httpRequestMono::subscribe, period);
+            return UpdateControl.noUpdate();
+        }
+
+        if (trafficScheduler.isScheduledTask(taskName)) {
+            log.warn("Task {} is already scheduled task.", taskName);
+            trafficTarget.updateTrafficTaskStatus(Status.FAILURE);
+        } else {
+            trafficScheduler.addFixedRateSchedule(taskName, httpRequestMono::subscribe, period);
+            trafficTarget.updateTrafficTaskStatus(Status.SCHEDULING);
+        }
+
         return UpdateControl.updateStatus(trafficTarget);
     }
 
@@ -36,16 +57,6 @@ public class TrafficMakerReconciler implements Reconciler<TrafficTarget> {
         var resourceName = trafficTarget.getMetadata().getName();
         trafficScheduler.removeSchedule(resourceName);
         return DeleteControl.defaultDelete();
-    }
-
-    @SuppressWarnings("ReactiveStreamsUnusedPublisher")
-    private void scheduleTask(TrafficTarget trafficTarget) {
-        var resourceName = trafficTarget.getMetadata().getName();
-        var httpTargetSpec = trafficTarget.getSpec().http();
-        var httpRequestMono = httpTargetSpec.toRequestMono(webClient);
-        var period = DurationStyle.detectAndParse(trafficTarget.getSpec().rate());
-        var isTaskScheduled = trafficScheduler.addFixedRateSchedule(resourceName, httpRequestMono::subscribe, period);
-        trafficTarget.updateTrafficTaskStatus(isTaskScheduled);
     }
 
 }
