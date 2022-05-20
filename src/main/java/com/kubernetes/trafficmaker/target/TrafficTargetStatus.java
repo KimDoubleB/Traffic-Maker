dbd67c573ccacb8a92fabe305733531ae601608d
KimDoubleB <tree9295@gmail.com>
2022-05-20 23:37:34 +0900

    refactor(#2): remove unused static methods

--- src/main/java/com/kubernetes/trafficmaker/target/TrafficTargetStatus.java
+++ src/main/java/com/kubernetes/trafficmaker/target/TrafficTargetStatus.java
@@ -1,17 +1,6 @@
 package com.kubernetes.trafficmaker.target;
 
 public record TrafficTargetStatus(Status status) {
-
-    public static boolean isScheduledStatus(TrafficTargetStatus trafficTargetStatus) {
-        return trafficTargetStatus != null
-               && trafficTargetStatus.status() == Status.SCHEDULING;
-    }
-
-    public static boolean isFailureStatus(TrafficTargetStatus trafficTargetStatus) {
-        return trafficTargetStatus != null
-               && trafficTargetStatus.status() == Status.FAILURE;
-    }
-
     public enum Status {
         SCHEDULING,
         FAILURE,
